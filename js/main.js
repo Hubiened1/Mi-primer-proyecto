78ee598a899a43ea4ef6ce5f43611718f59c2e53
Hubiened1 <hubiened@gmail.com>
2021-08-25 21:03:42 -0500

    Modificacion hecha 25-08-2021

--- js/main.js
+++ js/main.js
@@ -122,77 +122,193 @@
 
 // ****************Funciones******************
 
-function caminar(){
-    console.log('Estoy Caminando');
-}
-caminar();
-caminar();
+// function caminar(){
+//     console.log('Estoy Caminando');
+// }
+// caminar();
+// caminar();
 
-function contar (de, hasta){
-    console.log('Contando de '+ de + ' hasta '+ hasta);
-}
+// function contar (de, hasta){
+//     console.log('Contando de '+ de + ' hasta '+ hasta);
+// }
 
-contar(1,3);
+// contar(1,3);
 
-function sumar (num1, num2){
-    return num1+num2;
-}
-var total = sumar(1,2);
-console.log(total);
+// function sumar (num1, num2){
+//     return num1+num2;
+// }
+// var total = sumar(1,2);
+// console.log(total);
 
-console.log(typeof sumar);
-var yolo = sumar;
-var total2 = yolo(2,2);
-console.log(total2);
+// console.log(typeof sumar);
+// var yolo = sumar;
+// var total2 = yolo(2,2);
+// console.log(total2);
 
 
-// Funciones anonimas
+// // Funciones anonimas
 
-var password = function (num){
-    return num *4;
+// var password = function (num){
+//     return num *4;
 
-}
-console.log(password(2));
-console.log(password(1));
+// }
+// console.log(password(2));
+// console.log(password(1));
 
-// IIFE
+// // IIFE
 
-(function (sonrisa){
-    var seguro = 'seguro ';
-    console.log('Esto es '+ seguro + sonrisa);
-})(':)');
+// (function (sonrisa){
+//     var seguro = 'seguro ';
+//     console.log('Esto es '+ seguro + sonrisa);
+// })(':)');
 
-// caliback (llamada de vuelta
+// // caliback (llamada de vuelta
 
-function batman(edad){
-    console.log('Edad de Batman ' + edad);
-}
+// function batman(edad){
+//     console.log('Edad de Batman ' + edad);
+// }
 
-function superman(func){
-    console.log('Edad de superman 40');
-    func(39);
-}
+// function superman(func){
+//     console.log('Edad de superman 40');
+//     func(39);
+// }
 
-superman(batman);
+// superman(batman);
 
-// Objetos
+// // Objetos
 
-var persona = {
-    nombre: 'steven',
-    pasos: 0,
-    hablar: function (){
-        console.log(this.nombre + ' Esta hablando...');
-    },
-    caminar: function caminar(){
-        this.pasos++;
-        console.log(this.nombre + ' Esta caminando--- ' + this.pasos + ((this.pasos > 1) ? ' pasos' : ' paso'));
-    },
-    detener: function detener(){
-        console.log(this.nombre + ' esta congelado :)');
+// var persona = {
+//     nombre: 'steven',
+//     pasos: 0,
+//     hablar: function (){
+//         console.log(this.nombre + ' Esta hablando...');
+//     },
+//     caminar: function caminar(){
+//         this.pasos++;
+//         console.log(this.nombre + ' Esta caminando--- ' + this.pasos + ((this.pasos > 1) ? ' pasos' : ' paso'));
+//     },
+//     detener: function detener(){
+//         console.log(this.nombre + ' esta congelado :)');
+//     }
+// }
+
+// persona.hablar();
+// persona.caminar();
+// persona.detener();
+// persona.caminar();
+
+// *******************************ensayo*****************
+
+var hora;
+hora = 3;
+// if ((hora >= 4) && (hora <= 11)){
+//   console.log("Buenos dias");
+// }else if((hora >= 12)&&(hora <= 13)){
+//   console.log("Feliz almuerzo");
+// }else if ((hora >= 14)&&(hora <= 17)){
+//   console.log("Buenas tardes");
+// }else{
+//   console.log("Buenas noches");
+// }
+
+// var hora;
+// hora = 3;
+// switch (true){
+//   case (hora >= 4) && (hora <= 11):
+//   console.log("Muy buenos dias");
+//   break;
+//   case (hora >= 12)&&(hora <= 13):
+//   console.log("Feliz almuerzo");
+//   break;
+//   case (hora >= 14)&&(hora <= 17):
+//   console.log("Buenas tardes");
+//   break;
+//   default:
+//   console.log("Buenas noches");  
+// }
+// var militar = 11;
+// console.log("**********************")
+// militar = militar == 11 ? "hola":"no es"
+// console.log(militar);
+// console.log("*********************");
+// var d_nac;
+// var mes_nac;
+
+// d_nac = 25;
+// mes_nac = 11;
+// if (d_nac >= 21 && mes_nac == 3 || d_nac <= 20 && mes_nac == 4){
+//   console.log("Signo Aries");
+// }
+// else if (d_nac >= 21 && mes_nac == 4 || d_nac <= 20 && mes_nac == 5){
+//   console.log("Signo Tauro");
+// }
+// else if (d_nac >= 21 && mes_nac == 5 || d_nac <= 21 && mes_nac == 6){
+//   console.log("Signo Geminis");
+// }
+// else if (d_nac >= 22 && mes_nac == 6 || d_nac <= 22 && mes_nac == 7){
+//   console.log("Signo Cancer");
+// }
+// else if (d_nac >= 23 && mes_nac == 7 || d_nac <= 23 && mes_nac == 8){
+//   console.log("Signo Leo");
+// }
+// else if (d_nac >= 24 && mes_nac == 8 || d_nac <= 22 && mes_nac == 9){
+//   console.log("Signo Virgo");
+// }
+// else if (d_nac >= 23 && mes_nac == 9 || d_nac <= 22 && mes_nac == 10){
+//   console.log("Signo Libra");
+// } 
+// else if (d_nac >= 23 && mes_nac == 10 || d_nac <= 22 && mes_nac == 11){
+//   console.log("Signo Escorpio");
+// } 
+// else if (d_nac >= 23 && mes_nac == 11 || d_nac <= 21 && mes_nac == 12){
+//   console.log("Signo Sagitario");
+// } 
+// else if (d_nac >= 22 && mes_nac == 12 || d_nac <= 20 && mes_nac == 1){
+//   console.log("Signo Capricornio");
+// } 
+// else if (d_nac >= 21 && mes_nac == 1 || d_nac <= 19 && mes_nac == 2){
+//   console.log("Signo Acuario");
+// } 
+// else if (d_nac >= 20 && mes_nac == 2 || d_nac <= 20 && mes_nac == 3){
+//   console.log("Signo Piscis");
+// } 
+
+// var uno = 40;
+// var dos = 20;
+
+
+// // while(uno >= 20){
+// //   console.log(uno);
+// //   uno--;
+// // }
+
+// // do{
+// //     console.log(uno);
+// //     uno--;
+// // }while(uno >= 20)
+
+// for (var i = 1; i<=10; i++){
+//     if (i == 5 || i == 8){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// function uno(){
+    
+// }
+
+// var condicion = false;
+// while(condicion == true){
+//     console.log("hola");
+
+// }
+
+var n = 1;
+var m = 10;
+for(var inicio=n;final <= m; inicio++){
+    if(n%2 == 0 ){
+        console.log("numero par: ",n);
     }
+    
 }
-
-persona.hablar();
-persona.caminar();
-persona.detener();
-persona.caminar();
